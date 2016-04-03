be4e3fa4b667bc6abb1c651400710793ffe2b210
ANDREA STOCCO <stocco@uw.edu>
2016-04-03 13:19:22 -0700

    Better device

--- pss-simon-model.lisp
+++ pss-simon-model.lisp
@@ -4,19 +4,24 @@
 ;;; main idea, same as Lovett's NJAMOS
 ;;; Selective attention is competition among productions.
 
-
+(clear-all)
 (define-model pssimon
 
-(sgp :er t)
+(sgp :er t :esc T :auto-attend T)
 
 (chunk-type (simon-stimulus (:include visual-object))
+	    kind shape color position)
+
+(chunk-type (simon-stimulus-location (:include visual-object))
 	    shape color position)
+
 
 (chunk-type simon-rule kind has-motor-response shape hand)
 
 (chunk-type compatible-response has-motor-response hand position)
 
 (chunk-type wm kind value dimension)
+
 
 (add-dm (circle-left isa simon-rule
 		     kind simon-rule
@@ -35,7 +40,7 @@
 				   hand right
 				   position right)
 
-	(compatible-response-left isa compatble response
+	(compatible-response-left isa compatible-response
 				  has-motor-response yes
 				  hand left
 				  position left)
@@ -46,9 +51,28 @@
 		   color black
 		   kind simon-stimulus)
 
-
-	(wm1 isa wm kind proceed)
+	(wm1 isa wm
+	     kind proceed)
 )
+
+(p find-stimulus
+   ?visual>
+     buffer empty
+     state free
+==>
+   +visual-location>
+     kind simon-stimulus
+)  
+
+(p prepare-wm
+   ?imaginal>
+     buffer empty
+     state free
+==>
+   +imaginal>
+     isa wm
+     kind proceed
+)  
 
 
 (p process-shape
@@ -59,6 +83,7 @@
      kind proceed
      value nil 
  ==>
+   =visual>    
    =imaginal>
      dimension shape
      value =SHAPE
@@ -73,7 +98,8 @@
    ?retrieval>
      buffer empty
      state free
-==>
+ ==>
+   =imaginal>    
    +retrieval>
      has-motor-response yes
 )   
@@ -92,8 +118,15 @@
      hand =HAND
      finger index
 )      
-
-(set-buffer-chunk 'visual 'stimulus1)
-(set-buffer-chunk 'imaginal 'wm1)
-
 )
+;(set-buffer-chunk 'visual 'stimulus1)
+;(set-buffer-chunk 'imaginal 'wm1)
+
+(install-device '(shape circle color black position right))
+(proc-display)
+
+(defun my-reload ()
+  (clear-all)
+  (reload)
+  (install-device '(shape circle color black position right)))
+
--- simon-device.lisp
+++ simon-device.lisp
@@ -20,8 +20,6 @@
 
 (defparameter *ticks* 20)
 
-
-
 (defun bg-reward-hook (production reward time)
   (declare (ignore time))
 ;;  (print production)
@@ -36,28 +34,70 @@
 	  (t
 	   nil))))
 
+;;; A simon-trial of the form (shape circle position left)
 
-(defun simulate (n &optional (res-file "results.csv"))
-  (with-open-file (file res-file
-			   :direction :output
-			   :if-exists :overwrite
-			   :if-does-not-exist :create)
-    (let ((names (list 'd2 'ticks 'alpha 'egs 'accuracy 'problem 'choice)))
-      (format file "~{~a~^, ~}~%" names))
+(defun simon-trial? (lst)
+  (and (consp lst)
+       (evenp (length lst))))
 
-    (dolist (d2 '(1/4 1/2 3/4 1 5/4 3/2 7/4 2))
-      (dolist (ticks '(10 15 20 25 30 35 40))
-	(dolist (alpha '(0 2/10 4/10 6/10 8/10))
-	  (dolist (egs '(0 1/10 2/10 3/10 4/10 5/10))
-	    (format t "~{~a~^, ~}~%" (list 'd2 d2 'ans ans 'alpha alpha 'egs egs))
-	    (dotimes (j n)
-	      (rapm-reload)  ; Reload
-	      (setf *d2* d2)
-	      (sgp-fct `(:egs ,egs :ans ,ans :alpha alpha :v nil)) ; Sets the params
-	      (run 20 :real-time nil)
-	      (let* ((trial (first (experiment-log (current-device))))
-		     (res (list d2 ans alpha egs
-				(trial-accuracy trial)
-				(trial-problem-rt trial)
-				(trial-choice-rt trial))))
-		(format file "~{~a~^, ~}~%" res)))))))))
+(defmethod device-handle-keypress ((trial list) key)
+  "Converts the key into a symbol and passes it on to the task manager"
+  (declare (ignore trial key))
+  nil)
+			  
+
+			   
+(defmethod device-handle-click ((task list))
+  "Does nothing"
+  (declare (ignore task))
+  nil)
+
+(defmethod device-move-cursor-to ((task list) pos)
+  "Does nothing"
+  (declare (ignore task))
+  nil)
+
+
+(defmethod get-mouse-coordinates ((task list))
+  "Does nothing"
+  (declare (ignore task))
+  (vector 0 0))
+
+(defmethod cursor-to-vis-loc ((task list))
+  "Does nothing"
+  (declare (ignore task))
+  nil)
+
+
+(defmethod build-vis-locs-for ((trial list) vismod)
+  (let ((results nil))
+    (push  `(isa simon-stimulus-location 
+		 kind simon-stimulus
+		 value stimulus
+		 color black
+		 screen-x 0
+		 screen-y 0
+		 height 400 
+		 width 400
+		 ,@trial)
+	   results)
+    (define-chunks-fct results)))
+
+(defmethod vis-loc-to-obj ((task list) vis-loc)
+  "Transforms a visual-loc into a visual object"
+  (let ((kind (chunk-slot-value-fct vis-loc 'kind))
+	(new-chunk nil)
+	(trial task))
+    (cond ((equal kind 'simon-stimulus)
+	   
+	   ;; If the location was a cell
+
+	   (setf new-chunk
+		 (first (define-chunks-fct 
+			    `((isa simon-stimulus
+				   kind ,kind 
+				   ,@trial 
+				   )))))))
+    (fill-default-vis-obj-slots new-chunk vis-loc)
+    new-chunk))
+
