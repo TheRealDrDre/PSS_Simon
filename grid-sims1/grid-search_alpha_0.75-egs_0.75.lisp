0360c9b79a54a256b70afadcef0075724235c800
gardoma <andrea.stocco@gmail.com>
2016-12-06 00:18:28 -0800

    PSP sims initiated

--- grid-sims1/grid-search_alpha_0.75-egs_0.75.lisp
+++ grid-sims1/grid-search_alpha_0.75-egs_0.75.lisp
@@ -1,8 +1,8 @@
 
 (load "/projects/actr/actr7/load-act-r.lisp")
-(load "simon-device.lisp")
-(load "simon-model.lisp")
-(load "simon-simulations.lisp")
+(load "../simon-device.lisp")
+(load "../simon-model.lisp")
+(load "../simon-simulations.lisp")
 (with-open-file (out "grid-search-alpha_0.75-lf_0.75.txt" :direction :output 
 		     :if-exists :overwrite :if-does-not-exist :create)
   (simulate-psp 250
--- grid-sims1/grid-search_alpha_1.00-egs_1.00.lisp
+++ grid-sims1/grid-search_alpha_1.00-egs_1.00.lisp
@@ -1,8 +1,8 @@
 
 (load "/projects/actr/actr7/load-act-r.lisp")
-(load "simon-device.lisp")
-(load "simon-model.lisp")
-(load "simon-simulations.lisp")
+(load "../simon-device.lisp")
+(load "../simon-model.lisp")
+(load "../simon-simulations.lisp")
 (with-open-file (out "grid-search-alpha_1.00-lf_1.00.txt" :direction :output 
 		     :if-exists :overwrite :if-does-not-exist :create)
   (simulate-psp 250
