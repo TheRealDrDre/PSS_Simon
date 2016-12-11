00a45539aa96c9a64fc43aed29b86912712350d9
gardoma <andrea.stocco@gmail.com>
2016-12-10 21:40:44 -0800

    Final round 2

---
+++ grid-sims2/grid-search.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+TMPLT = """
+(load "/projects/actr/actr7/load-act-r.lisp")
+(load "../simon-device.lisp")
+(load "../simon-model.lisp")
+(load "../simon-simulations.lisp")
+(with-open-file (out "grid-search-alpha_%0.2f-lf_%0.2f.txt" :direction :output 
+		     :if-exists :overwrite :if-does-not-exist :create)
+  (simulate-psp 100
+		out
+                :alpha %0.2f
+                :lf %0.2f))
+		
+"""
+   
+if __name__ == "__main__":
+    i = 1
+    for alpha in [x/100.0 for x in range(0,101,25)]:
+        for lf in [x/100.0 for x in range(0,101,25)]:
+            fout = open("grid-search_alpha_%0.2f-egs_%0.2f.lisp" % (alpha, lf), 'w')
+            s = TMPLT % (alpha, lf, alpha, lf)
+            fout.write(s)
+            fout.flush()
+            fout.close()
+            i = i + 1
---
+++ grid-sims2/grid-search_alpha_0.00-egs_0.25.lisp
@@ -0,0 +1,12 @@
+
+(load "/projects/actr/actr7/load-act-r.lisp")
+(load "../simon-device.lisp")
+(load "../simon-model.lisp")
+(load "../simon-simulations.lisp")
+(with-open-file (out "grid-search-alpha_0.00-lf_0.25.txt" :direction :output 
+		     :if-exists :overwrite :if-does-not-exist :create)
+  (simulate-psp 100
+		out
+                :alpha 0.00
+                :lf 0.25))
+		
